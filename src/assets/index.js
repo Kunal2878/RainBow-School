0423414188134e22771bf803b5c8cee91f2fe3aa
Kunal <kunalpaul673@gmail.com>
2025-04-13 00:51:42 +0530

    First Commit

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import education from "./education.png";
+import onboarding from "./onboarding.jpg";
+import oops from "./oops.png"
+import rainbow from "./rainbow.png"
+export {education, rainbow, onboarding,oops};
